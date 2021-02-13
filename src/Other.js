241d71ceb994891ef335f8f1cc3f4206e3dd942f
unknown <mohammed.aseef04@gmail.com>
2021-02-13 12:14:42 +0530

    modified app.js added edit delay

--- src/Other.js
+++ src/Other.js
@@ -47,6 +47,12 @@ class Other extends Component {
             console.log(res.data);
           }
       );
+      setTimeout(function(){console.log("Delay") }, 2000);
+      axios.patch('https://frozen-cliffs-77329.herokuapp.com/memes/'+id, meme)
+          .then(res=>{
+            console.log(res.data);
+          }
+      );
       window.location.reload();
       
       
@@ -177,38 +183,7 @@ class Other extends Component {
                     </Modal.Footer>
                   </Modal>
                 </div> */}
-                                <div className="container">
-                                  {/* <!-- Button to Open the Modal --> */}
-                                  <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
-                                    Edit
-                                  </button>
-
-                                  {/* <!-- The Modal --> */}
-                                  <div className="modal fade" id="myModal">
-                                    <div className="modal-dialog modal-dialog-centered">
-                                      <div className="modal-content">
-                                      
-                                        {/* <!-- Modal Header --> */}
-                                        <div className="modal-header">
-                                          <h4 className="modal-title">Modal Heading</h4>
-                                          <button type="button" className="close" data-dismiss="modal">&times;</button>
-                                        </div>
-                                        
-                                        {/* <!-- Modal body --> */}
-                                        <div className="modal-body">
-                                          Modal body..
-                                        </div>
-                                        
-                                        {/* <!-- Modal footer --> */}
-                                        <div className="modal-footer">
-                                          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        </div>
-                                        
-                                      </div>
-                                    </div>
-                                  </div>
-                                  
-                                </div>
+                                
 
         </div>
     );
